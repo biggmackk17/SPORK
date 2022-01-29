da00fb9265518547108013bb89f945cb9919729c
biggmackk17 <austin.mackrell@gmail.com>
2022-01-28 21:01:21 -1000

    Player can move and jump, created a mat so I dont have to look at that awful unity base mat, added tags, imported sporkman

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float _speed;
+    [SerializeField]float _speed;
+    [SerializeField] float _jumpForce;
     Rigidbody _rb;
+    [SerializeField]bool _grounded  = true;
+
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        var xVel = Input.GetAxis("horizontal") * _speed;
-        var yVel = Input.GetAxis("vertical") * _speed;
+        var xVel = Input.GetAxis("Horizontal") * _speed;
+        var zVel = Input.GetAxis("Vertical") * _speed;
+        _rb.velocity = new Vector3(xVel, _rb.velocity.y, zVel);
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_grounded)
+            {
+                _rb.AddForce(Vector3.up*_jumpForce);
+            }
+        }
 
-        
+        Debug.DrawLine(transform.position, transform.position + Vector3.down,Color.red);
+        if(Physics.Raycast(transform.position,Vector3.down, out var hit, .25f))
+        {
+            Debug.Log(hit.transform.gameObject.name);
+            _grounded = true;
+        }
+        else
+        {
+            _grounded = false;
+        }
     }
 }
