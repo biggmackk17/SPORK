4e55c182b8847223e56cf6c1b7c38ebfacdec2e3
Daniel Schatzeder <Schatzederd@gmail.com>
2022-01-29 13:18:33 -1000

    Starter utensil script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,13 +11,14 @@ public class Enemy : MonoBehaviour, IDamageable
     private float _health;
     Animator _animator;
     private bool _alive;
+    private bool invincible = true;
 
     public enum EnemyType
     {
         SPOONABLE,
         FORKABLE
     }
-    [SerializeField] EnemyType _enemyType;
+    [SerializeField] public EnemyType _enemyType;
     // Start is called before the first frame update
 
     public enum EnemyState { 
@@ -42,14 +43,26 @@ public class Enemy : MonoBehaviour, IDamageable
             _rb.isKinematic = false;
             StartCoroutine(CollisionDelay());
         }
+        else if(collision.gameObject.layer == 7)
+        {
+            if(_enemyType == EnemyType.FORKABLE)
+            {
+                TakeDamage(10f);
+            }
+        }
     }
 
     public void TakeDamage(float amount)
     {
-        _health -= amount;
-        if (_health <= amount)
+        if (!invincible)
         {
-            _enemyState = EnemyState.DYING;
+            _health -= amount;
+            DamageCooldown();
+            Debug.Log(gameObject.name + " taking damage: " + amount + ":: remaining health: " + _health);
+            if (_health <= amount)
+            {
+                _enemyState = EnemyState.DYING;
+            }
         }
     }
 
@@ -59,6 +72,13 @@ public class Enemy : MonoBehaviour, IDamageable
         _agent.enabled = true;
         _rb.isKinematic = true;
 
+    }
+
+    private IEnumerator DamageCooldown()
+    {
+        invincible = true;
+        yield return new WaitForSeconds(1f);
+        invincible = false;
     }
 
     // Update is called once per frame
@@ -75,7 +95,7 @@ public class Enemy : MonoBehaviour, IDamageable
                 }
                 if (distance <= _attackDistance)
                 {
-                    //_enemyState = EnemyState.ATTACKING;
+                    _enemyState = EnemyState.ATTACKING;
                 }
                 break;
             case EnemyState.ATTACKING:
